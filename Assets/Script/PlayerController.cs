94c4c96d1eeeb7e506c2eb25e1f032f9e06ffe75
Chung Heon Lee <55828069+chungchunglee@users.noreply.github.com>
2021-06-26 07:16:56 +0900

    Player Control 스케치

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioClip deathClip;
+    public float jumpForce = 700f;
+
+    private int jumpCount = 0;
+    private bool isGrounded = false;
+    private bool isDead = false;
+    private Rigidbody2D playerrigidbody2D;
+    private Animator animator;
+    private AudioSource playerAudio;
+
+    private void Start()
+    {
+        playerrigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+    }
+    private void Update()
     {
         
+    }
+    private void Die()
+    {
+        //Á×À½ ±¸Çö
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //DeadÁ¸ Triggering
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //¹Ù´Ú °¨Áö
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        //¹Ù´Ú ¶³¾îÁü °¨Áö
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
